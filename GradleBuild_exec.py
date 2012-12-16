afebcbe49ec08027ee151da2a20fbec2fb28a47a
Matej Šircelj <sircelj.m@gmail.com>
2012-12-16 14:50:52 +0100

    initial commit

--- GradleBuild_exec.py
+++ GradleBuild_exec.py
@@ -2,9 +2,6 @@ import sublime, sublime_plugin, sys, os
 import subprocess
 import re
 
-DEFAULT_BUILD_CMD = "exec"
-DEFAULT_BUILD_TASK = "build"
-
 def preparelist(l):
     new_list = []
     for x in l:
@@ -15,19 +12,25 @@ def preparelist(l):
 
 class GradleBuildExecCommand(sublime_plugin.WindowCommand):
     def run(self, **kwargs):
+        self.buildScript = ""
         
-        package_dir = os.path.join(sublime.packages_path(), "GradleBuild");
-        self.build = os.path.join(package_dir, 'build.gradle')
-
-        path = None;
+        number_of_folders_in_project = len(self.window.folders())
+        if number_of_folders_in_project > 1:
+            print "There are more than one folders in project. Only the first \"gradle.build\" script found will be used!"
         if len(self.window.folders()) > 0:
             for folder in self.window.folders():
                 if os.path.exists(folder + os.sep + "build.gradle"):
-                    self.build = folder + os.sep + "build.gradle";
-                    self.folder = folder
+                    self.buildScript = folder + os.sep + "build.gradle";
+                    break
+
+        if self.buildScript == "":
+            print "Could not find build script!"
+            return
+
+        print self.buildScript
 
         try:
-            f = open(self.build);
+            f = open(self.buildScript);
         except Exception as ex:
             print ex;
             return 'The file could not be opened'
